ab3035025b97334daac5638eabf9c8b2403be375
cardaminexhz <zhxh69776@qq.com>
2021-06-28 18:04:51 +0800

    [贪心]435-Non-Overlapping Intervals

---
+++ algorithms/greedy/NonOverlappingIntervals_435.java
@@ -0,0 +1,19 @@
+package greedy;
+
+/**
+ * @Description TODO
+ * @Date 2021/6/28
+ */
+
+public class NonOverlappingIntervals_435 {
+
+    public static void main(String[] argus) {
+        int[][] intervals = {{1,2}, {2,3}, {1,3}};
+        System.out.println(eraseOverlapIntervals(intervals));
+    }
+
+    public static int eraseOverlapIntervals(int[][] intervals) {
+        return 0;
+
+    }
+}
