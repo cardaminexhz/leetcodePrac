991199d909e8c5fed34b3cacfb212f4d09433146
cardaminexhz <zhxh69776@qq.com>
2021-06-29 13:45:16 +0800

    [贪心]605 CanPlayFlowers

---
+++ algorithms/greedy/CanPlayFlowers_605.java
@@ -0,0 +1,45 @@
+package greedy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description
+ * 采取什么样的贪心策略，可以种植最多的花朵呢？
+ * @Date 2021/6/29
+ */
+
+public class CanPlayFlowers_605 {
+    public static void main(String[] argus) {
+        int[] flowerbed = {0};
+        int n = 4;
+        System.out.println(canPlaceFlowers(flowerbed, n));
+    }
+
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        List<Integer> idxs = new ArrayList<>();
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 1) {
+                idxs.add(i);
+            }
+        }
+
+        int sum = 0;
+        if(idxs.get(0) >= 2) {
+            sum += 1;
+            idxs.add(0, 0);
+        }
+        if(idxs.get(idxs.size()-1) <= flowerbed.length-1-2) {
+            sum += 1;
+            idxs.add(flowerbed.length-1);
+        }
+        for (int i = 1; i < idxs.size(); i++) {
+            int interval = idxs.get(i) - idxs.get(i - 1) - 1;
+            if (interval >= 3) {
+                sum += (interval - 1) / 2;
+            }
+        }
+
+        return (sum >= n);
+    }
+}
