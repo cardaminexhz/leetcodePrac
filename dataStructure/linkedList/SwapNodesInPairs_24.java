cb9c0c909a78e88b43548744d631923cce960f64
cardaminexhz <zhxh69776@qq.com>
2021-08-20 17:16:34 +0800

    [链表]24-SwapNodesInPairs

--- dataStructure/linkedList/SwapNodesInPairs_24.java
+++ dataStructure/linkedList/SwapNodesInPairs_24.java
@@ -1,5 +1,7 @@
 package linkedList;
 
+import java.util.Stack;
+
 /**
  * @Description TODO
  * @Tag
@@ -8,28 +10,28 @@ package linkedList;
 
 public class SwapNodesInPairs_24 {
     public static void main(String[] argus) {
-        int[] nums = {1, 2, 3, 4};
+        int[] nums = {1, 2, 3, 4,5,6};
         ListNode head = ListNode.initList(nums);
         head = swapPairs(head);
         ListNode.printList(head);
     }
 
     public static ListNode swapPairs(ListNode head) {
-        ListNode h = new ListNode();
-        h.next = head;
         ListNode cur = head;
-        ListNode prev = cur.next;
-        boolean ifFirst = true;;
-        while(cur.next != null) {
-            cur.next = prev.next;
-            prev.next = cur;
+        ListNode p = new ListNode();
+        head = p;
+        Stack<ListNode> stack = new Stack<>();
+        if(cur != null && cur.next !=null) {
+            stack.add(cur);
+            stack.add(cur.next);
 
-            head.next = prev;
-            head = head.next.next;
+            cur = cur.next.next;
 
-            cur = cur.next;
-            prev = cur.next;
+            p.next = stack.pop();
+            p = p.next;
+            p.next = stack.pop();
+            p = p.next;
         }
-        return head;
+        return head.next;
     }
 }
